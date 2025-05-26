c0c9c4b6ca915d154893806839359d69d1f5cef5
Iankennethgacusan <ganadoian31@gmail.com>
2025-05-26 22:12:45 +0800

    Update GroceryStacks.js

--- CS1A_FINExam_Gacusan/GroceryStacks.js
+++ CS1A_FINExam_Gacusan/GroceryStacks.js
@@ -1,1 +1,33 @@
+let queue = [];
 
+function showFront() {
+  if (queue.length === 0) {
+    console.log("Queue is empty.");
+  } else {
+    console.log("First Item:", queue[0]);
+  }
+}
+
+function enqueueItem(item) {
+  queue.push(item);
+  showFront();
+  console.log("Queue Now:", queue);
+}
+
+function dequeueItem() {
+  if (queue.length > 0) {
+    queue.shift();
+    showFront();
+    console.log("Updated Queue:", queue);
+  } else {
+    console.log("Nothing to dequeue.");
+  }
+}
+
+for (let i = 0; i < 5; i++) {
+  let item = prompt(`Enter task ${i + 1}:`);
+  enqueueItem(item);
+}
+
+// Example dequeue
+dequeueItem();
